a7901f1aa17693689b0323063b50cb5c959a6e87
김경식 <0345tlr@naver.com>
2022-06-03 14:04:39 +0900

    goormlevel_1 ( 정사각형의 개수 )

---
+++ Coding_Test/Coding_Test7.cpp
@@ -0,0 +1,26 @@
+﻿/*
+*
+* 구름 Level 정사각형의 개수
+*/
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	long lCnt = 0;
+	long lNum;
+
+	cin >> lNum;
+
+	// N^2 + (N-1)^2 + (N-2)^2 + ... + (N-M)^2 + 1
+	for (long i = lNum; i > 0; --i) {
+		lCnt += (i * i);
+	}
+
+	cout << lCnt;
+	return 0;
+}
+*/
+
