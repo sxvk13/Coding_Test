7954fc1ffb5912573c7635800b1c574b3567cc39
김경식 <0345tlr@naver.com>
2022-09-29 15:25:43 +0900

    Programmers Lv1 하샤드 수

---
+++ Coding_Test/Coding_Test17.cpp
@@ -0,0 +1,31 @@
+/*
+*
+*	Programmers Lv1 하샤드 수
+*/
+/*#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+
+    string str = to_string(x);
+    int iSum = 0;
+    for (auto i : str) {
+        iSum += (int)i - '0';
+    }
+
+
+    return answer = (x % iSum) == 0 ? true:false;
+}
+auto main()->int {
+
+    int X;
+    cin >> X;
+    bool result = solution(X);
+
+    cout << result;
+
+}*/
