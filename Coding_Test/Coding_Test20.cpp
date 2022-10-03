15af3c476bde9271fea545700d0191bef3b30ded
김경식 <0345tlr@naver.com>
2022-10-03 16:32:01 +0900

    Programmers Lv1 x만큼 간격이 있는 n개의 숫자

---
+++ Coding_Test/Coding_Test20.cpp
@@ -0,0 +1,36 @@
+/*
+*
+*	Programmers Lv1 x만큼 간격이 있는 n개의 숫자
+*
+*
+*/
+/*
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+
+    for (int i = 1; i <= n; ++i) {
+        answer.push_back(i * x);
+    }
+
+    return answer;
+}
+
+auto main()->int {
+    int X, N;
+
+    cin >> X >> N;
+    vector<long long> result = solution(X, N);
+    
+    for (int r : result) {
+         cout << r<<" ";
+    }
+
+}
+*/
